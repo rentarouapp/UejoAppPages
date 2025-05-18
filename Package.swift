edf1646b5e74710c8550909c88a961ea07ae8718
uejo <rentarou1994@gmail.com>
2025-05-18 13:58:19 +0900

    :tada: Initial Commit!!

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "UejoAppPages",
+    platforms: [.macOS(.v12)],
+    products: [
+        .executable(
+            name: "UejoAppPages",
+            targets: ["UejoAppPages"]
+        )
+    ],
+    dependencies: [
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.8.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "UejoAppPages",
+            dependencies: ["Publish"]
+        )
+    ]
+)
